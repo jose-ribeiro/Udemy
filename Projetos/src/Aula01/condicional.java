9d1f70a3a5ae38c0041970e56774d8a0d6f40723
jose-ribeiro <williamribeiro419@gmail.com>
2021-03-28 20:05:34 -0400

    Aula01  Curso Udemy

---
+++ Projetos/src/Aula01/condicional.java
@@ -0,0 +1,23 @@
+package Aula01;
+
+import java.util.Scanner;
+
+public class condicional {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double time = sc.nextDouble();
+		
+		if(time <12) {
+			System.out.println("Good morning ");
+		}else if (time <=12 && time <18); {
+			System.out.println("Good afternoon");
+		}
+		if(time>=18) {
+			System.out.println("good everning");
+		}
+		sc.close();
+	}	
+		 
+
+}
---
+++ Projetos/src/Aula01exemplo/exercicio3.java
@@ -0,0 +1,20 @@
+package Aula01exemplo;
+import java.util.Scanner;
+public class exercicio3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); 
+		
+		int A,B,C,D, Difernca;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+		Difernca = A*B - C*D;
+		
+		System.out.println("Diferen�a = " + Difernca);
+		
+		sc.close();
+	}
+
+}
