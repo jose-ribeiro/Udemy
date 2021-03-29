72f31458775cafbbda0c0da6d98b909595379ce9
jose-ribeiro <williamribeiro419@gmail.com>
2021-03-28 20:15:48 -0400

    Aula01 Udemy

---
+++ src/Aula01/Programa.java
@@ -0,0 +1,10 @@
+package Aula01;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		System.out.println("Ola mundo");
+		System.out.println();
+	}
+
+}
---
+++ src/Aula01exemplo/exercicio1.java
@@ -0,0 +1,18 @@
+package Aula01exemplo;
+import java.util.Scanner;
+
+public class exercicio1 {
+	
+	public static void main(String[] args) {
+		
+		  Scanner sc = new Scanner(System.in);
+		  int primen = sc.nextInt();
+		  int segun = sc.nextInt();
+		  int soma = primen + segun;
+		  System.out.println("soma = " + soma);
+		    
+		  
+		  sc.close();
+	}
+
+}
